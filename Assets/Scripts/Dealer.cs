82b06b033a4ca080332709d92407285acf4498be
Charles Wolfe <c.wolfe132@gmail.com>
2022-03-19 20:24:35 -0700

    deals player cards and table cards on screen

--- Assets/Scripts/Dealer.cs
+++ Assets/Scripts/Dealer.cs
@@ -7,8 +7,13 @@ using UnityEngine;
 public class Dealer : MonoBehaviour
 {
     
-    [SerializeField] List<string> players;
+    [SerializeField] List<string> opponents;
+    [SerializeField] Sprite[] cardImages;
+    [SerializeField] Transform[] playerCardPositions;
+    [SerializeField] Transform[] tableCardPositions;
     
+    private GameObject playerCard1, playerCard2;
+
     List<string> activePlayers = new List<string>();
     Dictionary<string, List<int>> playerHands = new Dictionary<string, List<int>>();
 
@@ -17,11 +22,12 @@ public class Dealer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        InitializeActivePlayers();
         DealHands();
-        Deal(3);
-        Deal();
-        Deal();
-        PrintHands();
+        DealFlop();
+        DealTurn();
+        DealRiver();
+        //PrintHands();
     }
 
     // Update is called once per frame
@@ -30,9 +36,22 @@ public class Dealer : MonoBehaviour
         
     }
 
+    private void InitializeActivePlayers()
+    {
+        activePlayers.Add("Player");
+
+        foreach(string opponent in opponents)
+        {
+            activePlayers.Add(opponent);
+        }
+    }
+
     private void DealHands()
     {
-        foreach(string player in players)
+        DealPlayerHand();
+        //int playerCardPosIdx = 0;
+
+        foreach(string player in opponents)
         {
             List<int> cards = new List<int>();
 
@@ -48,6 +67,62 @@ public class Dealer : MonoBehaviour
         }
     }
 
+    private void DealPlayerHand()
+    {
+        List<int> cards = new List<int>();
+
+        int card1 = Random.Range(0, deck.Count);
+        cards.Add(deck[card1]);
+        playerCardPositions[0].GetComponent<SpriteRenderer>().sprite = cardImages[card1];
+        deck.RemoveAt(card1);
+
+        int card2 = Random.Range(0, deck.Count);
+        cards.Add(deck[card2]);
+        playerCardPositions[1].GetComponent<SpriteRenderer>().sprite = cardImages[card2];
+        deck.RemoveAt(card2);
+
+        playerHands.Add("Player", cards);
+    }
+
+    private void DealFlop()
+    {
+        // get random card from deck, display image, store card in each players hand, then remove from deck
+        int card1 = Random.Range(0, deck.Count);
+        tableCardPositions[0].GetComponent<SpriteRenderer>().sprite = cardImages[card1];
+        foreach (string player in activePlayers) { playerHands[player].Add(deck[card1]); }
+        deck.RemoveAt(card1);
+
+        // get random card from deck, display image, store card in each players hand, then remove from deck
+        int card2 = Random.Range(0, deck.Count);
+        tableCardPositions[1].GetComponent<SpriteRenderer>().sprite = cardImages[card2];
+        foreach (string player in activePlayers) { playerHands[player].Add(card2); }
+        deck.RemoveAt(card2);
+
+        // get random card from deck, display image, store card in each players hand, then remove from deck
+        int card3 = Random.Range(0, deck.Count);
+        tableCardPositions[2].GetComponent<SpriteRenderer>().sprite = cardImages[card3];
+        foreach (string player in activePlayers) { playerHands[player].Add(card3); }
+        deck.RemoveAt(card3);
+    }
+
+    private void DealTurn()
+    {
+        // get random card from deck, display image, store card in each players hand, then remove from deck
+        int card = Random.Range(0, deck.Count);
+        tableCardPositions[3].GetComponent<SpriteRenderer>().sprite = cardImages[card];
+        foreach (string player in activePlayers) { playerHands[player].Add(deck[card]); }
+        deck.RemoveAt(card);
+    }
+
+    private void DealRiver()
+    {
+        // get random card from deck, display image, store card in each players hand, then remove from deck
+        int card = Random.Range(0, deck.Count);
+        tableCardPositions[4].GetComponent<SpriteRenderer>().sprite = cardImages[card];
+        foreach (string player in activePlayers) { playerHands[player].Add(deck[card]); }
+        deck.RemoveAt(card);
+    }
+
     private void Deal(int times = 1)
     {
         int n = 0;
@@ -55,7 +130,7 @@ public class Dealer : MonoBehaviour
         {
             int card = Random.Range(0, deck.Count);
 
-            foreach (string player in players)
+            foreach (string player in opponents)
             {
                 playerHands[player].Add(deck[card]);
             }
